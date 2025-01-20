6fe3beedf1589ee719ed8913e11ca8bf32320b51
Andr3sPA <andres.pena2@udea.edu.co>
2025-01-20 17:35:28 -0500

    add insertClothes function

--- back/internal/database/database.go
+++ back/internal/database/database.go
@@ -1,58 +1,85 @@
 package database
 
 import (
-    "context"
-    "log"
-    "os"
-    "time"
+	"context"
+	"log"
+	"os"
+	"time"
 
-    _ "github.com/joho/godotenv/autoload"
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	_ "github.com/joho/godotenv/autoload"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Service interface {
-    Health() map[string]string
-    CountDocuments(filter bson.D) (int64, error)
+	Health() map[string]string
+	CountDocuments(filter bson.D) (int64, error)
+	InsertClothes(restaurants []Clothing) ([]interface{}, error)
 }
 
 type service struct {
-    db *mongo.Client
+	db *mongo.Client
 }
 
 func New() Service {
-    uri := os.Getenv("BLUEPRINT_DB_URI")
+	uri := os.Getenv("BLUEPRINT_DB_URI")
 
-    client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
-    if err != nil {
-        log.Fatal(err)
-    }
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    return &service{
-        db: client,
-    }
+	return &service{
+		db: client,
+	}
 }
 
 func (s *service) Health() map[string]string {
-    ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-    defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
 
-    err := s.db.Ping(ctx, nil)
-    if err != nil {
-        log.Fatalf("db down: %v", err)
-    }
+	err := s.db.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("db down: %v", err)
+	}
 
-    return map[string]string{
-        "message": "It's healthy",
-    }
+	return map[string]string{
+		"message": "It's healthy",
+	}
 }
 
 func (s *service) CountDocuments(filter bson.D) (int64, error) {
-    coll := s.db.Database("sample_mflix").Collection("movies")
-    count, err := coll.CountDocuments(context.Background(), filter)
-    if err != nil {
-        return 0, err
-    }
-    return count, nil
+	coll := s.db.Database("e_commerce").Collection("clothes")
+	count, err := coll.CountDocuments(context.Background(), filter)
+	if err != nil {
+		return 0, err
+	}
+	return count, nil
+}
+
+type Clothing struct {
+    Name        string  `bson:"name,omitempty"`         
+    Size        string  `bson:"size,omitempty"`         
+    Color       string  `bson:"color,omitempty"`        
+    Price       float64 `bson:"price,omitempty"`     
+    Material    string  `bson:"material,omitempty"`     
+    Description string  `bson:"description,omitempty"`  
+}
+
+
+func (s *service) InsertClothes(clothes []Clothing) ([]interface{}, error) {
+	coll := s.db.Database("e_commerce").Collection("clothes")
+
+	var docs []interface{}
+	for _, item := range clothes {
+		docs = append(docs, item)
+	}
+
+	result, err := coll.InsertMany(context.TODO(), docs)
+	if err != nil {
+		return nil, err
+	}
+
+	return result.InsertedIDs, nil
 }
--- back/internal/server/clothes.go
+++ back/internal/server/clothes.go
@@ -1,22 +1,42 @@
 package server
 
 import (
-    "net/http"
+	"back/internal/database" // Importa el paquete database
+	"net/http"
 
-    "github.com/gin-gonic/gin"
-    "go.mongodb.org/mongo-driver/bson"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (s *Server) clothesHandler(c *gin.Context) {
-    filter := bson.D{{"year", 1916}}
-    count, err := s.db.CountDocuments(filter)
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-        return
-    }
+	filter := bson.D{}
+	count, err := s.db.CountDocuments(filter)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 
-    resp := make(map[string]interface{})
-    resp["message"] = "Hello Clothes"
-    resp["count"] = count
-    c.JSON(http.StatusOK, resp)
+	resp := make(map[string]interface{})
+	resp["message"] = "Hello Clothes"
+	resp["count"] = count
+	c.JSON(http.StatusOK, resp)
+}
+
+func (s *Server) insertClothes(c *gin.Context) {
+	var newClothes []database.Clothing // Usa un slice de Restaurant del paquete database
+
+	// Call BindJSON to bind the received JSON to newRestaurants.
+	if err := c.BindJSON(&newClothes); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Aquí puedes insertar los nuevos restaurantes en la base de datos
+	insertedIDs, err := s.db.InsertClothes(newClothes)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"inserted_ids": insertedIDs})
 }
